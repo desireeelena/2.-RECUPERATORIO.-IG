88d41d0bdea4a2784120896a295e1873c2c9cf3a
desireeelena <desireefigueroaq24@gmail.com>
2026-09-12 22:30:44 -0300

    Galeria de obras
    Armado inicial

---
+++ js/1_unavozsingular.js
@@ -0,0 +1,30 @@
+// Obras de Laurie Anderson
+const obras = [
+    {
+        archivo: "img/heartofadog.jpg",
+        nombre: "Heart of a Dog",
+        anio: 2015,
+    },
+    {
+        archivo: "img/habeascorpus.jpeg",
+        nombre: "Habeas Corpus",
+        anio: 1985,
+    },
+    {
+        archivo: "img/osuperman.jpg",
+        nombre: "O Superman",
+        anio: 1981,
+    },
+    {
+        archivo: "img/moon.jpg",
+        nombre: "The End of the Moon",
+        anio: 2002,
+    },
+    { 
+         archivo: "img/united-states.jpg",
+         nombre: "United States I-IV",
+         anio: 1983,
+    }
+];
+
+
