5fb1a88e1f21871e6238ce5e3dfec9c5f165447d
desireeelena <desireefigueroaq24@gmail.com>
2026-09-13 14:52:06 -0300

    Página 3 - Archivo musical

---
+++ js/3_ejercicio.js
@@ -0,0 +1,11 @@
+// Ejercicio 3: archivo musical
+
+let cantidadObras = 0;
+let obras = [];
+
+let formCantidad = document.querySelector("#formCantidad");
+let btnComenzar = document.querySelector("#btnComenzar");
+let seccionObras = document.querySelector("#seccionObras");
+let btnGuardarObra = document.querySelector("#btnGuardarObra");
+
+}
